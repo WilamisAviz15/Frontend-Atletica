94322e682e7eccd78221885529c88da81700c041
Wilamis Micael de Araujo Aviz <wmaa@ic.ufal.br>
2023-08-27 15:38:33 -0300

    feat: add component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import "./App.css";
+import Home from "./pages/home";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
