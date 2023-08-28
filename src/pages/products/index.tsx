c92cedd953164827e85dde2d9798db0aac8f911a
Kelvin Almeida <58817347+kelvinalmeida@users.noreply.github.com>
2023-08-28 10:53:35 -0300

    up

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import ProdutoCard from "./card/product_card";
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 
 const Products = () => {
-  return <h1>Produtos disponíveis</h1>;
+  return (<>
+    {/* <h1>Produtos disponíveis</h1>
+    <ProdutoCard /> */}
+
+    <Grid container spacing={2} margin={2} width={'80%'} justifyContent={'center'}>
+      <Grid item xs={3}>
+        <ProdutoCard />
+      </Grid>
+      <Grid item xs={3}>
+        <ProdutoCard />
+      </Grid>
+      <Grid item xs={3}>
+        <ProdutoCard />
+      </Grid>
+      <Grid item xs={3}>
+        <ProdutoCard />
+      </Grid>
+      <Grid item xs={3}>
+        <ProdutoCard />
+      </Grid>
+      <Grid item xs={3}>
+        <ProdutoCard />
+      </Grid>
+      <Grid item xs={3}>
+        <ProdutoCard />
+      </Grid>
+      <Grid item xs={3}>
+        <ProdutoCard />
+      </Grid>
+    </Grid >
+
+  </>)
 };
 
 export default Products;
