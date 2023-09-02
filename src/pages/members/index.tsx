183a1d9749659affc3f5b0b09fb15617e47b336c
Kelvin Almeida <58817347+kelvinalmeida@users.noreply.github.com>
2023-09-02 11:52:37 -0300

    membros update

--- src/pages/members/index.tsx
+++ src/pages/members/index.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+
+import man1 from './img/man1.jpg';
+import man2 from './img/man2.jpg';
+
+function ImageAvatars() {
+  return (
+    <Stack direction="row" spacing={2}>
+      <Avatar alt="Remy Sharp" src={man1} sx={{ width: 100, height: 100 }} />
+      <Avatar alt="Remy Sharp" src={man2} sx={{ width: 100, height: 100 }} />
+    </Stack>
+  );
+}
+
 const Members = () => {
-  return <h1>Membros da atlética</h1>;
+  return <>
+    <h1>Membros da atlética</h1>
+    <ImageAvatars />
+  </>
 };
 
 export default Members;
