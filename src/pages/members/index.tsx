b19482b31cabf8dbffb52c71634283238f8c22c8
Kelvin Almeida <58817347+kelvinalmeida@users.noreply.github.com>
2023-09-04 14:42:48 -0300

    membros update

--- src/pages/members/index.tsx
+++ src/pages/members/index.tsx
@@ -18,30 +18,69 @@ import woman6 from './img/woman6.jpg';
 
 function ImageAvatars() {
   return (<>
-    <Stack direction="row" spacing={4}>
-      <Avatar alt="Remy Sharp" src={man2} sx={{ width: 100, height: 100 }} />
-      <Avatar alt="Remy Sharp" src={woman1} sx={{ width: 100, height: 100 }} />
-      <Avatar alt="Remy Sharp" src={woman3} sx={{ width: 100, height: 100 }} />
-      <Avatar alt="Remy Sharp" src={man3} sx={{ width: 100, height: 100 }} />
-      <Avatar alt="Remy Sharp" src={woman2} sx={{ width: 100, height: 100 }} />
-      <Avatar alt="Remy Sharp" src={man4} sx={{ width: 100, height: 100 }} />
+    <Stack direction="column" alignItems={'center'} marginTop={5} width={'100%'}>
+      <h1>Membros da atlética</h1>
+      <Stack direction="row" marginTop={5} spacing={6}>
+        <div className="peaple">
+          <Avatar alt="Remy Sharp" src={man2} sx={{ width: '10vw', height: '10vw' }} />
+          <p>Antônio</p>
+        </div>
+        <div className="peaple">
+          <Avatar alt="Remy Sharp" src={woman1} sx={{ width: '10vw', height: '10vw' }} />
+          <p>Maria</p>
+        </div>
+        <div className="peaple">
+          <Avatar alt="Remy Sharp" src={woman3} sx={{ width: '10vw', height: '10vw' }} />
+          <p>Eduarda</p>
+        </div>
+        <div className="peaple">
+          <Avatar alt="Remy Sharp" src={man3} sx={{ width: '10vw', height: '10vw' }} />
+          <p>João</p>
+        </div>
+        <div className="peaple">
+          <Avatar alt="Remy Sharp" src={woman2} sx={{ width: '10vw', height: '10vw' }} />
+          <p>Alice</p>
+        </div>
+        <div className="peaple">
+          <Avatar alt="Remy Sharp" src={man4} sx={{ width: '10vw', height: '10vw' }} />
+          <p>Pedro</p>
+        </div>
+      </Stack>
+      <Stack direction="row" spacing={6} marginTop={5} >
+        <div className="peaple">
+          <Avatar alt="Remy Sharp" src={woman6} sx={{ width: '10vw', height: '10vw' }} />
+          <p>Poliana</p>
+        </div>
+        <div className="peaple">
+          <Avatar alt="Remy Sharp" src={woman5} sx={{ width: '10vw', height: '10vw' }} />
+          <p>Geovana</p>
+        </div>
+        <div className="peaple">
+          <Avatar alt="Remy Sharp" src={man1} sx={{ width: '10vw', height: '10vw' }} />
+          <p>Lucas</p>
+        </div>
+        <div className="peaple">
+          <Avatar alt="Remy Sharp" src={man5} sx={{ width: '10vw', height: '10vw' }} />
+          <p>Fabrício</p>
+        </div>
+        <div className="peaple">
+          <Avatar alt="Remy Sharp" src={woman4} sx={{ width: '10vw', height: '10vw' }} />
+          <p>Ana</p>
+        </div>
+        <div className="peaple">
+          <Avatar alt="Remy Sharp" src={man6} sx={{ width: '10vw', height: '10vw' }} />
+          <p>Marcos</p>
+        </div>
+      </Stack>
+    </Stack >
 
-    </Stack>
-    <Stack direction="row" spacing={4}>
-      <Avatar alt="Remy Sharp" src={woman6} sx={{ width: 100, height: 100 }} />
-      <Avatar alt="Remy Sharp" src={woman5} sx={{ width: 100, height: 100 }} />
-      <Avatar alt="Remy Sharp" src={man1} sx={{ width: 100, height: 100 }} />
-      <Avatar alt="Remy Sharp" src={man5} sx={{ width: 100, height: 100 }} />
-      <Avatar alt="Remy Sharp" src={woman4} sx={{ width: 100, height: 100 }} />
-      <Avatar alt="Remy Sharp" src={man6} sx={{ width: 100, height: 100 }} />
-    </Stack>
   </>
   );
 }
 
 const Members = () => {
   return <>
-    <h1>Membros da atlética</h1>
+
     <ImageAvatars />
   </>
 };
