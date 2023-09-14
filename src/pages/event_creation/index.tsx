88b8e5558ffd199320f7dbd8eac78234379426dc
Kelvin Almeida <58817347+kelvinalmeida@users.noreply.github.com>
2023-09-14 11:23:40 -0300

    evento up

--- src/pages/event_creation/index.tsx
+++ src/pages/event_creation/index.tsx
@@ -1,46 +1,34 @@
 import React, { useEffect, useState } from "react";
+import "./event.css";
 
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 
-
-import { AdapterMomentHijri } from '@mui/x-date-pickers/AdapterMomentHijri';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import useTheme from '@mui/system/useTheme';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import { FormControl } from "@mui/material";
 
 function EventCreation() {
     return (
-        <FormControl sx={{
-            '& > :not(style)': { m: 1, backgroundColor: "#456efe", width: '99vw', flexDirection: 'column', justifyContent: "center", alignItems: "center" },
-        }}>
-            <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': { m: 1, backgroundColor: "#c1eeea", flexDirection: 'column', justifyContent: "center", alignItems: "center" },
-                }}
-                noValidate
-                autoComplete="off"
-            >
 
-                <p>Titulo do Evento</p>
-                <TextField id="outlined-basic" label="Titulo do Evento" variant="outlined" />
+        <form className="form">
+            <div className="FormEvent">
+                <h1>Criar Evento</h1>
+                <h2>Titulo do Evento</h2>
+                <TextField className="textField" id="outlined-basic" label="Titulo do Evento" variant="outlined" />
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <p>Data do Evento</p>
-                    <DatePicker />
-                    <p>Hora do Evento</p>
-                    <TimePicker />
+                    <h2>Data do Evento</h2>
+                    <DatePicker className="picker" />
+                    <h2>Hora do Evento</h2>
+                    <TimePicker className="picker" />
                 </LocalizationProvider>
-                <p>Informações complementares sobre o evento!</p>
-                <TextField id="outlined-basic" label="Informações do Evento" variant="outlined" />
-            </Box>
-        </FormControl >
+                <h2>Informações complementares sobre o evento!</h2>
+                <TextField className="textField" id="outlined-basic" label="Informações do Evento" variant="outlined" />
+                <Button className="button" variant="contained">Criar Evento</Button>
+            </div>
+        </form >
         // 
 
     );
